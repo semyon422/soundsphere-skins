f62d439b58f52254a6bd68fce3253cf1a64b3169
semyon422 <semyon422@users.noreply.github.com>
2022-05-23 14:58:54 +0500

    Fix and refactor piano skin

--- soundsphere piano/88key.skin.lua
+++ soundsphere piano/88key.skin.lua
@@ -2,7 +2,6 @@ local NoteSkinVsrg = require("sphere.models.NoteSkinModel.NoteSkinVsrg")
 local BasePlayfield = require("sphere.models.NoteSkinModel.BasePlayfield")
 
 local noteskin = NoteSkinVsrg:new({
-	path = ...,
 	name = "piano",
 	inputMode = "88key",
 	range = {-1, 1},
@@ -16,20 +15,62 @@ for i = 1, 88 do
 end
 noteskin:setInput(input)
 
-noteskin.align = "center"
-
-local keys = {3, 1, 4, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 3, 1, 5, 1, 6, 3, 1, 7, 1, 8, 1, 6, 2}
-local images = {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0}
-local w = {6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 6, 4, 6, 4, 6, 4, 6, 6}
-local x = {0, 3, 6, 12, 15, 18, 23, 24, 30, 33, 36, 40, 42, 47, 48, 54, 57, 60, 65, 66, 72, 75, 78, 82, 84, 89, 90, 96, 99, 102, 107, 108, 114, 117, 120, 124, 126, 131, 132, 138, 141, 144, 149, 150, 156, 159, 162, 166, 168, 173, 174, 180, 183, 186, 191, 192, 198, 201, 204, 208, 210, 215, 216, 222, 225, 228, 233, 234, 240, 243, 246, 250, 252, 257, 258, 264, 267, 270, 275, 276, 282, 285, 288, 292, 294, 299, 300, 306}
-
-for i = 1, #w do
-	x[i] = x[i] * 320 / 52
-	w[i] = w[i] * 320 / 52
+local function append(t1, t2)
+	for i, v in ipairs(t2) do
+		t1[#t1 + 1] = v
+	end
+	return t1
 end
 
-noteskin.columns = x
-noteskin.width = w
+local images = {0, 1, 0}
+for _ = 1, 7 do
+	append(images, {0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0})
+end
+images[#images + 1] = 0
+
+local w = {}
+for i = 1, #images do
+	w[i] = images[i] == 0 and 6 or 4
+end
+
+local blackKeysOffsets = {-1}
+for _ = 1, 7 do
+	append(blackKeysOffsets, {-1, 1, -1, 0, 1})
+end
+local keyToOffset = {}
+local c = 0
+for i = 1, #images do
+	if images[i] == 1 then
+		c = c + 1
+		keyToOffset[i] = blackKeysOffsets[c]
+	end
+end
+
+local x = {}
+local cb = 0
+local cw = 0
+for i = 1, #images do
+	local x_i = cw * 6
+	if images[i] == 0 then
+		cw = cw + 1
+	elseif images[i] == 1 then
+		cb = cb + 1
+		x_i = x_i - 2 + blackKeysOffsets[cb]
+	end
+	x[i] = x_i
+end
+
+for i = 1, #w do
+	x[i] = x[i] * 1920 / 312
+	w[i] = w[i] * 1920 / 312
+end
+
+noteskin:setColumns({
+	offset = 0,
+	align = "center",
+	width = w,
+	position = x,
+})
 
 local keysImages = {
 	"keyBlack.png",
@@ -39,15 +80,42 @@ local keysImages = {
 	"keyWhite3.png",
 	"keyWhite4.png",
 	"keyWhite5.png",
-	"keyWhite6.png"
+	"keyWhite6.png",
 }
+local function getKeyName(i)
+	if images[i] == 1 then
+		return 1
+	end
+	if i == 88 then
+		return 2
+	elseif i == 1 then
+		return 3
+	end
 
+	local prev = keyToOffset[i - 1]
+	local next = keyToOffset[i + 1]
+	if not prev and next == -1 then
+		return 3
+	elseif prev == -1 and not next then
+		return 4
+	elseif prev == -1 and next == 1 then
+		return 5
+	elseif prev == 1 and not next then
+		return 6
+	elseif prev == -1 and next == 0 then
+		return 7
+	elseif prev == 0 and next == 1 then
+		return 8
+	end
+end
+
+local keys  ={}
 for i = 1, #w do
-	keys[i] = keysImages[keys[i]]
+	keys[i] = keysImages[getKeyName(i)]
 end
 
 noteskin:setTextures({
-	{measure = "white.png"},
+	{measure = ""},
 	{clearB = "clearB.png"},
 	{darkB = "darkB.png"},
 	{clearT = "clearT.png"},
@@ -58,17 +126,7 @@ noteskin:setTextures({
 	{dark = "dark.png"},
 })
 
-noteskin:setImages({
-	measure = {"white"},
-	clearB = {"clearB"},
-	darkB = {"darkB"},
-	clearT = {"clearT"},
-	darkT = {"darkT"},
-	clearH = {"clearH"},
-	darkH = {"darkH"},
-	clear = {"clear"},
-	dark = {"dark"},
-})
+noteskin:setImagesAuto()
 
 local short = {}
 for i, image in ipairs(images) do
@@ -100,30 +158,20 @@ noteskin:addMeasureLine({
 	image = "measure"
 })
 
-noteskin:addBga({
-	x = 0,
-	y = 0,
-	w = 1,
-	h = 1,
-	color = {0.25, 0.25, 0.25, 1}
-})
-
 local playfield = BasePlayfield:new({
 	noteskin = noteskin
 })
 
-local tf = playfield:newTransform(1920, 1080, "left")
+local tf = playfield:newFullTransform(1920, 1080)
+tf[1] = {1 / 2, 0}
 
-playfield:addBga({
-	transform = {{1 / 2, -16 / 9 / 2}, {0, -7 / 9 / 2}, 0, {0, 16 / 9}, {0, 16 / 9}, 0, 0, 0, 0}
-})
 playfield:enableCamera()
 playfield:addNotes({
 	transform = tf
 })
 playfield:add({
 	class = "ImageView",
-	x = 0, y = 1080 - 200, w = 1920, h = 200,
+	x = -960, y = 1080 - 200, w = 1920, h = 200,
 	transform = tf,
 	image = "Piano.png",
 })
